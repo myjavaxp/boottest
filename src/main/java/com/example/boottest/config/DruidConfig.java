27f813a7c59888c27580ddeca06e29502d2dc02a
Yibo <xuyibo@outlook.com>
2018-11-04 16:20:12 +0800

    改用FastJson

--- src/main/java/com/example/boottest/config/DruidConfig.java
+++ src/main/java/com/example/boottest/config/DruidConfig.java
@@ -8,6 +8,11 @@ import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * 这个版本的druid已经可以把这些配置写入yml文件了
+ *
+ * @author Yibo
+ */
 @Configuration
 public class DruidConfig {
     @Value("${spring.datasource.username}")
