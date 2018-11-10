bd7e46a98b24dcbc9e4d044eb202d02f0fec17e5
Yibo <xuyibo@outlook.com>
2018-11-10 11:12:57 +0800

    多线程操作

--- src/test/java/com/example/boottest/concurrency/ConcurrencyTest.java
+++ src/test/java/com/example/boottest/concurrency/ConcurrencyTest.java
@@ -1,6 +1,7 @@
 package com.example.boottest.concurrency;
 
 import com.example.boottest.annotation.NotThreadSafe;
+import com.example.boottest.annotation.ThreadSafe;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,6 +10,7 @@ import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Semaphore;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @NotThreadSafe
 public class ConcurrencyTest {
@@ -16,8 +18,10 @@ public class ConcurrencyTest {
     private static final int CLIENT_TOTAL = 5000;
     private static final int THREAD_TOTAL = 200;
     private static int count = 0;
+    private static AtomicInteger atomicCount = new AtomicInteger(0);
 
     @Test
+    @NotThreadSafe
     public void test01() {
         ExecutorService threadPool = Executors.newCachedThreadPool();
         final Semaphore semaphore = new Semaphore(THREAD_TOTAL);
@@ -41,5 +45,46 @@ public class ConcurrencyTest {
         }
         threadPool.shutdown();
         LOGGER.info("count:{}", count);
+    }
+
+    @Test
+    @ThreadSafe
+    public void test02() {
+        ExecutorService threadPool = Executors.newCachedThreadPool();
+        final Semaphore semaphore = new Semaphore(THREAD_TOTAL);
+        final CountDownLatch countDownLatch = new CountDownLatch(CLIENT_TOTAL);
+        for (int i = 0; i < CLIENT_TOTAL; i++) {
+            threadPool.execute(() -> {
+                try {
+                    semaphore.acquire();
+                    /*
+                    调用链---
+                    public final int incrementAndGet() {
+                        return unsafe.getAndAddInt(this, valueOffset, 1) + 1;
+                    }
+                    //var1 原始对象，var2，当前线程的对象初始值，var4，需要加的值
+                    public final int getAndAddInt(Object var1, long var2, int var4) {
+                        int var5;
+                        do {
+                            var5 = this.getIntVolatile(var1, var2);
+                        } while(!this.compareAndSwapInt(var1, var2, var5, var5 + var4));
+                        return var5;
+                    }
+                     */
+                    atomicCount.incrementAndGet();
+                    semaphore.release();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                countDownLatch.countDown();
+            });
+        }
+        try {
+            countDownLatch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        threadPool.shutdown();
+        LOGGER.info("atomicCount:{}", atomicCount.get());
     }
 }
